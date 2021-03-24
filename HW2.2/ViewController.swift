71dcb89b56f1274db27a52f06a07b10f5a0cb3e9
Anatoliy <tolxpams@gmail.com>
2021-03-24 21:13:22 +0200

    without keyboard

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewImage: UIView!
+    
+    @IBOutlet weak var redCount: UILabel!
+    @IBOutlet weak var greenCount: UILabel!
+    @IBOutlet weak var blueCount: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func colorChange() {
+        viewImage.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
+    @IBAction func sliderChange(_ sender: Any) {
+        colorChange()
+        redCount.text = String (redSlider.value)
+        greenCount.text = String (greenSlider.value)
+        blueCount.text = String (blueSlider.value)
+    }
+    
 
 }
 
